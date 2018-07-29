1dbf9e5ade2236d7eaacde6bd4365e159c977121
Sergii Horoshko <sergii.horoshko@mev.com>
2018-07-29 17:38:30 +0300

    hotFix for querie 2.2

--- queries/2.2.sql
+++ queries/2.2.sql
@@ -3,6 +3,6 @@ USE mevtwitter;
 SELECT AVG(
 (YEAR(CURRENT_DATE) - YEAR(birthday)) - 
 
-(DATE_FORMAT(CURRENT_DATE, '%m%d') > DATE_FORMAT(birthday, '%m%d'))
+(DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birthday, '%m%d'))
 
 ) AS middle_age from users;
